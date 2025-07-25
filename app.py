a47d1e06533cd3c270be1b2a1f37aba6c3e476c9
Habeba Mostafa <Habiba.Mostafa.55@h-eng.helwan.edu.eg>
2025-07-25 12:10:58 +0300

    ى

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def load_translation_models():
 # تهيئة نموذج المحادثة
 @st.cache_resource
 def load_model():
-    model_name = "mistralai/Mistral-7B-Instruct-v0.1"
+    model_name = "mistralai/Mistral-7B-v0.1"
     
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(
